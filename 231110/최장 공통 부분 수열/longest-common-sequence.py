f18940a5ef2bc5b306fbabd355ddb09485d9238d
chaeyeongyun <70565663+chaeyeongyun@users.noreply.github.com>
2023-11-10 11:42:43 +0900

    Title: 최장 공통 부분 수열, Time: 367ms, Memory: 52MB, Status: Passed - CodeTree

---
+++ 231110/최장 공통 부분 수열/longest-common-sequence.py
@@ -0,0 +1,24 @@
+import sys
+MAX_INT = sys.maxsize
+def initialize():
+    global dp, first, second
+    
+
+def solution():
+    global first, second, dp
+    dp = [[0] * (len(second) + 1) for _ in range(len(first) + 1)]
+    for i in range(1, len(first) + 1):
+        for j in range(1, len(second) + 1):
+            fw, sw = first[i - 1], second[j - 1]
+            if fw == sw:
+                dp[i][j] = dp[i-1][j-1] + 1
+            else:
+                dp[i][j] = max(dp[i-1][j-1], dp[i - 1][j], dp[i][j - 1])
+    print(max(sum(dp, [])))
+    # for d in dp:
+    #     print(d)
+
+if __name__ == "__main__":
+    first = input().rstrip()
+    second = input().rstrip()
+    solution()
