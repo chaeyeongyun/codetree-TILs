a560901c2d2588292d484a55a1bdb4bf264a688e
chaeyeongyun <70565663+chaeyeongyun@users.noreply.github.com>
2023-11-17 11:34:24 +0900

    Title: 두 수의 합, Time: 144ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 231117/두 수의 합/sum-of-two-num.py
@@ -0,0 +1,18 @@
+from collections import defaultdict
+n, k = map(int, input().rstrip().split(" "))
+nums = list(map(int, input().rstrip().split(" ")))
+
+count = defaultdict(list)
+for i in range(n):
+    count[nums[i]].append(i)
+ans = 0
+visited = defaultdict(lambda: False)
+for number in nums:
+    if visited[number]:
+        continue
+    if (k - number) not in count:
+        continue
+    ans += len(count[number]) * len(count[k - number])
+    visited[number] = True
+    visited[k - number] = True
+print(ans)
