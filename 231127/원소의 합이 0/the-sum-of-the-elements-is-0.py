fabce70cdaa482716ac6fec410f90f4e4185c229
chaeyeongyun <70565663+chaeyeongyun@users.noreply.github.com>
2023-11-27 16:46:25 +0900

    Title: 원소의 합이 0, Time: 284ms, Memory: 39MB, Status: Passed - CodeTree

--- 231127/원소의 합이 0/the-sum-of-the-elements-is-0.py
+++ 231127/원소의 합이 0/the-sum-of-the-elements-is-0.py
@@ -1,22 +1,34 @@
-from collections import defaultdict
-def solution():
-    global n, a_list, b_list, c_list, d_list
-    ab_counter, cd_counter = defaultdict(int), defaultdict(int)
-    # a, b / c, d로 나누어서 두개씩?
-    # O(n^2)
-    for i in range(n):
-        for j in range(n):
-            ab_counter[a_list[i] + b_list[j]] += 1
-            cd_counter[c_list[i] + d_list[j]] += 1
-    answer = 0
-    for ab in ab_counter:
-        answer += ab_counter[ab] * cd_counter[-ab]
-    print(answer)
+# 변수 선언 및 입력:
+n = int(input())
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+C = list(map(int, input().split()))
+D = list(map(int, input().split()))
 
-if __name__ == "__main__":
-    n = int(input().rstrip())
-    a_list = list(map(int, input().rstrip().split(" ")))
-    b_list = list(map(int, input().rstrip().split(" ")))
-    c_list = list(map(int, input().rstrip().split(" ")))
-    d_list = list(map(int, input().rstrip().split(" ")))
-    solution()
+count = dict()
+
+ans = 0
+
+# A 수열에서 숫자 하나, B 수열에서 숫자 하나를 골랐을 때
+# 나올 수 있는 두 숫자의 합들을 hashmap에 기록해줍니다.
+for i in range(n):
+    for j in range(n):
+        sum_val = A[i] + B[j]
+        if sum_val in count:
+            count[sum_val] += 1
+        else:
+            count[sum_val] = 1
+
+# C, D 수열을 순회하며 쌍을 만들어줍니다.
+# 앞서 계산한 hashmap을 이용하면
+# C, D 수열에서 고른 값으로 A, B와 쌍을 만들 때
+# 총합이 0이 되는 쌍의 개수를 쉽게 구할 수 있습니다.
+for i in range(n):
+    for j in range(n):
+        diff = - C[i] - D[j]
+
+        if diff in count:
+            ans += count[diff]
+
+# 출력:
+print(ans)
