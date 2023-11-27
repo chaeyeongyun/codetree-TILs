26172cf19d47a4fa98f7c75930cfd4ae9b76ceb2
chaeyeongyun <70565663+chaeyeongyun@users.noreply.github.com>
2023-11-27 19:02:53 +0900

    Title: C-TAG, Time: 913ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 231127/C-TAG/c-tag.py
@@ -0,0 +1,27 @@
+def test_location(x, y, z):
+    global n, m, a_group, b_group
+    # x, y, z 세 자리 조합 선택했을 때 A와 B가 구분가는지
+    # 구분가면 True, 아니면 False를 리턴
+    s = set()
+    for i in range(n):
+        s.add(a_group[i][x] + a_group[i][y] + a_group[i][z])
+    for i in range(n):
+        if (b_group[i][x] + b_group[i][y] + b_group[i][z]) in s:
+            return False
+    return True
+
+def solution():
+    global n, m, a_group, b_group
+    answer = 0
+    for i in range(m):
+        for j in range(i + 1, m):
+            for k in range(j + 1, m):
+                if test_location(i, j, k):
+                    answer += 1
+    print(answer)
+
+if __name__ == "__main__":
+    n, m = map(int, input().rstrip().split(" "))
+    a_group = [input().rstrip() for _ in range(n)]
+    b_group = [input().rstrip() for _ in range(n)]
+    solution()
