452bbc68dc62210bcd6ef5ffe510059b2d9e1d58
chaeyeongyun <70565663+chaeyeongyun@users.noreply.github.com>
2023-11-27 17:17:31 +0900

    Title: 특별한 문자, Time: 144ms, Memory: 25MB, Status: Passed - CodeTree

--- 231127/특별한 문자/special-character.py
+++ 231127/특별한 문자/special-character.py
@@ -1,13 +1,17 @@
 from collections import OrderedDict
 
 word = input().rstrip()
-counter = OrderedDict()
-for w in word:
-    if w in counter:
-        counter[w] += 1
-    else:
-        counter[w] = 1
-for w in counter:
-    if counter[w] == 1:
-        print(w)
-        break
+def solution():
+    global word
+    counter = OrderedDict()
+    for w in word:
+        if w in counter:
+            counter[w] += 1
+        else:
+            counter[w] = 1
+    for w in counter:
+        if counter[w] == 1:
+            print(w)
+            return
+    print(None)
+solution()
