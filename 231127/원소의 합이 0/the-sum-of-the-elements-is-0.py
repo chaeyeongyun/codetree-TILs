99687f9debd3d00d1b3469e3d57306920b18109b
chaeyeongyun <70565663+chaeyeongyun@users.noreply.github.com>
2023-11-27 16:44:18 +0900

    Title: 원소의 합이 0, Time: 447ms, Memory: 67MB, Status: Passed - CodeTree

--- 231127/원소의 합이 0/the-sum-of-the-elements-is-0.py
+++ 231127/원소의 합이 0/the-sum-of-the-elements-is-0.py
@@ -3,12 +3,12 @@ def solution():
     global n, a_list, b_list, c_list, d_list
     # n**4개의 경우의 수 있음
     # 요소의 개수 체크 O(n)
-    a_counter, b_counter, c_counter, d_counter = defaultdict(int), defaultdict(int), defaultdict(int), defaultdict(int)
-    for i in range(n):
-        a_counter[a_list[i]] += 1
-        b_counter[b_list[i]] += 1
-        c_counter[c_list[i]] += 1
-        d_counter[d_list[i]] += 1
+    # a_counter, b_counter, c_counter, d_counter = defaultdict(int), defaultdict(int), defaultdict(int), defaultdict(int)
+    # for i in range(n):
+    #     a_counter[a_list[i]] += 1
+    #     b_counter[b_list[i]] += 1
+    #     c_counter[c_list[i]] += 1
+    #     d_counter[d_list[i]] += 1
     ab_counter, cd_counter = defaultdict(int), defaultdict(int)
     # a, b / c, d로 나누어서 두개씩?
     for i in range(n):
@@ -17,7 +17,7 @@ def solution():
             cd_counter[c_list[i] + d_list[j]] += 1
     answer = 0
     for ab in ab_counter:
-        answer += cd_counter[-ab]
+        answer += ab_counter[ab] * cd_counter[-ab]
     print(answer)
 
 if __name__ == "__main__":
