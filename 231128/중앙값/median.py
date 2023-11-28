30504a2e5e7d808047da83aec2e2ffca47a3bca5
chaeyeongyun <70565663+chaeyeongyun@users.noreply.github.com>
2023-11-28 18:39:35 +0900

    Title: 중앙값, Time: 383ms, Memory: 31MB, Status: Wrong Answer - CodeTree

--- 231128/중앙값/median.py
+++ 231128/중앙값/median.py
@@ -1,18 +1,37 @@
 from heapq import heappop, heappush
 t = int(input().rstrip())
 for _ in range(t):
-    max_heap, min_heap = [], []
+    left_max_heap, right_min_heap = [], []
     m = int(input().rstrip())
     nums = list(map(int, input().rstrip().split(" ")))
-    answer = []
-    for i in range(m):
-        heappush(max_heap, -nums[i])
-        heappush(min_heap, nums[i])
-        if i % 2 == 0:
-            max_temp, min_temp = max_heap[:], min_heap[:]
-            for _ in range(len(max_temp)):
-                ma, mi = -heappop(max_temp), heappop(min_temp)
-                if ma == mi:
-                    answer.append(str(ma))
-                    break
-    print(" ".join(answer))
+    mid = nums[0]
+    mid_list = [mid]
+    for i in range(m // 2):
+        a, b = nums[2 * i + 1], nums[2 * i + 2]
+        cnt = 0
+        if a < mid:
+            cnt += 1
+            heappush(left_max_heap, -a)
+        else:
+            heappush(right_min_heap, a)
+        if b < mid:
+            cnt += 1
+            heappush(left_max_heap, -b)
+        else:
+            heappush(right_min_heap, b)
+        if cnt == 2:
+            # case 1. mid보다 작은값 두개가 들어왔을 때
+            # left_max_heap에거 가장 큰 값이 mid가 되고 원래 mid는 right_min_heap에 들어간다
+            heappush(right_min_heap, mid)
+            mid = -heappop(left_max_heap)
+        elif cnt == 0:
+            # case 2. mid보다 큰 값 두개가 들어왔을 때
+            # right_min_heap에거 가장 작은 값이 mid가 되고 원래 mid는 left_max_heap에 들어간다
+            heappush(left_max_heap, mid)
+            mid = heappop(right_min_heap)
+        elif cnt == 1:
+            # case 3. mid보다 큰 값 하나, 작은 값 하나 들어왔을때
+            # mid는 그대로
+            pass
+        mid_list.append(mid)
+    print(*mid_list)
