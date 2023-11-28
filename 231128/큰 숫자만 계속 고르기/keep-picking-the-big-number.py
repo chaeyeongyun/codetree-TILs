48f43426e2c62f9f256abd82aaaa13b5d08f598e
chaeyeongyun <70565663+chaeyeongyun@users.noreply.github.com>
2023-11-28 16:50:21 +0900

    Title: 큰 숫자만 계속 고르기, Time: 181ms, Memory: 32MB, Status: Passed - CodeTree

--- 231128/큰 숫자만 계속 고르기/keep-picking-the-big-number.py
+++ 231128/큰 숫자만 계속 고르기/keep-picking-the-big-number.py
@@ -1,13 +1,25 @@
 # 1. 최대힙
-from heapq import heappop, heappush
+# from heapq import heappop, heappush
+# n, m = map(int, input().rstrip().split(" "))
+# temp = list(map(int, input().rstrip().split(" ")))
+# nums = []
+# for num in temp:
+#     heappush(nums, -num)
+# for _ in range(m):
+#     x = -heappop(nums)
+#     x -= 1
+#     heappush(nums, -x)
+# answer = -nums[0]
+# print(answer)
+
+# 2. 이진탐색
+from bisect import bisect_left
 n, m = map(int, input().rstrip().split(" "))
-temp = list(map(int, input().rstrip().split(" ")))
-nums = []
-for num in temp:
-    heappush(nums, -num)
+nums = list(map(int, input().rstrip().split(" ")))
+nums.sort()
 for _ in range(m):
-    x = -heappop(nums)
+    x = nums.pop()
     x -= 1
-    heappush(nums, -x)
-answer = -nums[0]
-print(answer)
+    idx = bisect_left(nums, x)
+    nums.insert(idx, x)
+print(nums[-1])
