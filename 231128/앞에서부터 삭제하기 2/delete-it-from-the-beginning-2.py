9ee2ac5a5e2419345957b984615c831410988c9c
chaeyeongyun <70565663+chaeyeongyun@users.noreply.github.com>
2023-11-28 17:49:26 +0900

    Title: 앞에서부터 삭제하기 2, Time: 5010ms, Memory: 34MB, Status: Time Limit Exceed - CodeTree

--- 231128/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.py
+++ 231128/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.py
@@ -6,8 +6,11 @@ nums = list(map(int, input().rstrip().split(" ")))
 answer = MIN_INT
 for k in range(1, n - 1):
     temp = nums[k:]
-    temp.sort()
-    mean = sum(temp[1:]) / (len(temp) - 1)
+    min_val = 10000
+    for v in temp:
+        if v < min_val:
+            min_val = v
+    mean = (sum(temp) - min_val) / (len(temp) - 1)
     answer = max(mean, answer)
 
 print("%.2f"%(answer))
