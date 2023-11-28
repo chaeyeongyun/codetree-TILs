878f30437c51b14c231af73b99eb0d5aa27d2504
chaeyeongyun <70565663+chaeyeongyun@users.noreply.github.com>
2023-11-28 15:22:20 +0900

    Title: 대칭 차집합, Time: 267ms, Memory: 51MB, Status: Passed - CodeTree

--- 231128/대칭 차집합/symmetric-difference-set.py
+++ 231128/대칭 차집합/symmetric-difference-set.py
@@ -1,7 +1,9 @@
 num_a, num_b = map(int, input().rstrip().split(" "))
 set_a = set(input().rstrip().split(" "))
 set_b = set(input().rstrip().split(" "))
-a_minus_b = set_a - set_b
-b_minus_a = set_b - set_a
-answer = len(a_minus_b.union(b_minus_a))
+
+answer = num_a + num_b
+for elem in set_a:
+    if elem in set_b:
+        answer -= 2
 print(answer)
