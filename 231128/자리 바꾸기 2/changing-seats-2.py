911b6b140eaae91be802192cfe88ec21282db33f
chaeyeongyun <70565663+chaeyeongyun@users.noreply.github.com>
2023-11-28 14:24:52 +0900

    Title: 자리 바꾸기 2, Time: 843ms, Memory: 68MB, Status: Passed - CodeTree

---
+++ 231128/자리 바꾸기 2/changing-seats-2.py
@@ -0,0 +1,28 @@
+def solution():
+    global n, k, change
+    pos_record = dict() 
+    positions = dict()
+    for i in range(1, n + 1):
+        positions[i] = i
+        pos_record[i] = {i}
+
+    for _ in range(3):
+        for i in range(k):
+            a, b = change[i]
+            temp = positions[a]
+
+            positions[a] = positions[b]
+            positions[b] = temp
+            pos_record[positions[a]].add(a)
+            pos_record[positions[b]].add(b)
+            
+    for i in range(1, n + 1):
+        print(len(pos_record[i]))
+
+
+if __name__ == "__main__":
+    n, k = map(int, input().rstrip().split(" "))
+    change = []
+    for _ in range(k):
+        change.append(tuple(map(int, input().rstrip().split(" "))))
+    solution()
