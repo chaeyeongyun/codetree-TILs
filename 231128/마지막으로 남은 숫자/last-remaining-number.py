47af67ffe7eaf781c09154634d9d55fa71bc1e08
chaeyeongyun <70565663+chaeyeongyun@users.noreply.github.com>
2023-11-28 17:33:52 +0900

    Title: 마지막으로 남은 숫자, Time: 308ms, Memory: 30MB, Status: Wrong Answer - CodeTree

---
+++ 231128/마지막으로 남은 숫자/last-remaining-number.py
@@ -0,0 +1,15 @@
+# 정렬, 제거, 삽입 반복
+from heapq import heappop, heappush, heapify
+n = int(input().rstrip())
+nums = list(map(lambda x: -int(x), input().rstrip().split(" ")))
+heapify(nums)
+while len(nums) >= 2:
+    a = heappop(nums)
+    b = heappop(nums)
+    diff = abs(a - b)
+    if diff > 0:
+        heappush(nums, diff)
+if nums:
+    print(nums[0])
+else:
+    print(-1)
