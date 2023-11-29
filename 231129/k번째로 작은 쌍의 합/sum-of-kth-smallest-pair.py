09e69ffd73655395e33279f43c75daa3eb65e4fe
chaeyeongyun <70565663+chaeyeongyun@users.noreply.github.com>
2023-11-29 17:44:58 +0900

    Title: k번째로 작은 쌍의 합, Time: 5040ms, Memory: 30MB, Status: Time Limit Exceed - CodeTree

---
+++ 231129/k번째로 작은 쌍의 합/sum-of-kth-smallest-pair.py
@@ -0,0 +1,34 @@
+from heapq import heappop, heappush
+import sys
+MAX_INT = sys.maxsize
+
+def solution():
+    global n, m, j, a_list, b_list
+    q = []
+    answer = MAX_INT
+    for i in range(n):
+        for j in range(m):
+            if len(q) < (k - 1):
+                heappush(q, -(a_list[i] + b_list[j]))
+                continue
+            k_1 = -heappop(q)
+            cand = a_list[i] + b_list[j]
+            if cand < answer and cand > k_1:
+                answer = cand
+                heappush(q, -k_1)
+            elif cand <= k_1:
+                answer = k_1
+                heappush(q, -cand)
+            elif cand >= answer:
+                heappush(q, -k_1)
+    print(answer)
+            
+
+            
+
+
+if __name__ == "__main__":
+    n, m, k = map(int, input().rstrip().split(" "))
+    a_list = list(map(int, input().rstrip().split(" ")))
+    b_list = list(map(int, input().rstrip().split(" ")))
+    solution()
