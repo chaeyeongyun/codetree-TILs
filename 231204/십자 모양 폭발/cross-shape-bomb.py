80b678b10c51e5a723637726c78a1789bc32bf7a
chaeyeongyun <70565663+chaeyeongyun@users.noreply.github.com>
2023-12-04 16:23:09 +0900

    Title: 십자 모양 폭발, Time: 146ms, Memory: 26MB, Status: Passed - CodeTree

--- 231204/십자 모양 폭발/cross-shape-bomb.py
+++ 231204/십자 모양 폭발/cross-shape-bomb.py
@@ -1,24 +1,23 @@
 def solution():
     global n, grid, r, c
     bomb_size = grid[r][c]
-    temp_grid = [y[:] for y in grid]
-    for i in range(r + 1):
-        for j in range(max(0, c - bomb_size + 1), min(c + bomb_size - 1, n - 1) + 1):
-            if j == c:
-                continue
-            if i == 0:
-                grid[i][j] = 0
-            else:
-                grid[i][j] = temp_grid[i - 1][j]
-    for i in range(min(n, r + bomb_size)):
-        if i >= 2 * bomb_size - 1:
-            grid[i][c] = temp_grid[i - 2 * bomb_size + 1][c]
-        else:
-            grid[i][c] = 0
-    for g in grid:
-        print(*g)
+    next_grid = [[0] * n for _ in range(n)]
+    # 터질 위치 0으로 채우기
+    for j in range(max(0, c - bomb_size + 1), min(n, c + bomb_size)):
+        grid[r][j] = 0
+    for i in range(max(0, r - bomb_size + 1), min(n, r + bomb_size)):
+        grid[i][c] = 0
     
-
+    # 터진 곳에 값 채우기
+    for j in range(n):
+        target_row = n - 1 # 맨 아래칸부터 확인
+        for i in range(n - 1, -1, -1):
+            if grid[i][j]: # grid에 0이 아닌 값이 있으면 그 값을 맨 아래칸부터 채우게된다.
+                next_grid[target_row][j] = grid[i][j]
+                target_row -= 1
+    
+    for g in next_grid:
+        print(*g)
 
 if __name__ == "__main__":
     n = int(input().rstrip())
