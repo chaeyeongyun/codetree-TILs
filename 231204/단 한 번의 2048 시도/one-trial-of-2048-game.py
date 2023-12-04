7f7cb0e36cea710ac882e85c1e54c6642827d23b
chaeyeongyun <70565663+chaeyeongyun@users.noreply.github.com>
2023-12-04 18:45:31 +0900

    Title: 단 한 번의 2048 시도, Time: 109ms, Memory: 24MB, Status: Passed - CodeTree

--- 231204/단 한 번의 2048 시도/one-trial-of-2048-game.py
+++ 231204/단 한 번의 2048 시도/one-trial-of-2048-game.py
@@ -1,49 +1,9 @@
-def right():
+def rotate90():
     global grid
     next_grid = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
     for i in range(4):
-        next_idx = 3
-        for j in range(3, -1, -1):
-            if grid[i][j]:
-                for k in range(j - 1, -1, -1):
-                    if grid[i][k]:
-                        break
-                if j >= 1 and grid[i][k] == grid[i][j]:
-                    grid[i][j], grid[i][k] = 2 * grid[i][j], 0
-                next_grid[i][next_idx] = grid[i][j]
-                next_idx -= 1
-    return next_grid
-
-def left():
-    global grid
-    next_grid = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
-    for i in range(4):
-        next_idx = 0
         for j in range(4):
-            if grid[i][j]:
-                for k in range(j + 1, 4):
-                    if grid[i][k]:
-                        break
-                if j <= 2 and grid[i][k] == grid[i][j]:
-                    grid[i][j], grid[i][k] = 2 * grid[i][j], 0
-                next_grid[i][next_idx] = grid[i][j]
-                next_idx += 1
-    return next_grid
-
-def up():
-    global grid
-    next_grid = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
-    for j in range(4):
-        next_idx = 0
-        for i in range(4):
-            if grid[i][j]:
-                for k in range(i + 1, 4):
-                    if grid[k][j]:
-                        break
-                if i <= 2 and grid[k][j] == grid[i][j]:
-                    grid[i][j], grid[k][j] = 2 * grid[i][j], 0
-                next_grid[next_idx][j] = grid[i][j]
-                next_idx += 1
+            next_grid[j][3 - i] = grid[i][j]
     return next_grid
 
 def down():
@@ -69,16 +29,28 @@ def down():
 def solution():
     global grid, direction
     if direction == "R":
-        next_grid = right()
+        grid = rotate90()
+        grid = down()
+        for _ in range(3):
+            grid = rotate90()
     elif direction == "L":
-        next_grid = left()
+        for _ in range(3):
+            grid = rotate90()
+        grid = down()
+        grid = rotate90()
     elif direction == "D":
-        next_grid = down()
+        grid = down()
+        
     elif direction == "U":
-        next_grid = up()
+        for _ in range(2):
+            grid = rotate90()
+        grid = down()
+        for _ in range(2):
+            grid = rotate90()
     else:
         raise ValueError
-    for g in next_grid:
+    
+    for g in grid:
         print(*g)
 
 if __name__ == "__main__":
