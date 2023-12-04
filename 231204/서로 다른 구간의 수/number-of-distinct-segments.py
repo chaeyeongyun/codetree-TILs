1de4f541bf034af6d69173c2300f39c781e68981
chaeyeongyun <70565663+chaeyeongyun@users.noreply.github.com>
2023-12-04 14:41:56 +0900

    Title: 서로 다른 구간의 수, Time: 370ms, Memory: 39MB, Status: Passed - CodeTree

--- 231204/서로 다른 구간의 수/number-of-distinct-segments.py
+++ 231204/서로 다른 구간의 수/number-of-distinct-segments.py
@@ -3,20 +3,16 @@ n = int(input().rstrip())
 # a, b는 최대 10**9이므로 배열로 +1-1테크닉 수행은 불가
 # 시작점, 끝점을 저장하여 수행
 checks = defaultdict(int)
-start, end = 0, 0
 for _ in range(n):
     # 주어지는 모든 좌표값은 다름
     a, b = map(int, input().rstrip().split(" "))
     checks[a] += 1
     checks[b] -= 1
-    start = min(start, a, b)
-    end = max(end, a, b)
 
 answer = 0
 temp = 0
-for i in range(start, end + 1):
-    if i not in checks:
-        continue
+idxs = sorted(list(checks.keys()))
+for i in idxs:
     temp += checks[i]
     if temp == 0:
         answer += 1
