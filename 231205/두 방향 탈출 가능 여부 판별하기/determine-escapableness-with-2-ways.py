c6fa698d07fbefbe7f9741431dec2ee07351b547
chaeyeongyun <70565663+chaeyeongyun@users.noreply.github.com>
2023-12-05 15:04:57 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 114ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231205/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
+++ 231205/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
@@ -6,25 +6,23 @@ def in_range(nx, ny):
     return 0 <= nx < n and 0 <= ny < n
 
 def dfs(i, j):
-    global n
-    ret = False
+    global n, answer
     if i == n - 1 and j == n - 1:
-        ret = True
+        answer = True
     # 오른쪽 혹은 아래쪽만 이동 가능
     dx = [0, 1]
     dy = [1, 0]
     for k in range(2):
         nx, ny = i + dx[k], j + dy[k]
         if in_range(nx, ny) and grid[nx][ny] == 1:
-            ret = dfs(nx, ny)
-    return ret
-
+            dfs(nx, ny)
 
 if __name__ == "__main__":
     n, m = map(int, input().rstrip().split(" "))
     grid = [list(map(int, input().rstrip().split(" "))) for _ in range(n)]
-    ret = dfs(0, 0)
-    if ret:
+    answer = False
+    dfs(0, 0)
+    if answer:
         print(1)
     else:
         print(0)
