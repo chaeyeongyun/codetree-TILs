ff7447982c244cfe282e5a3004f54a84d4d25382
chaeyeongyun <70565663+chaeyeongyun@users.noreply.github.com>
2023-12-08 16:00:36 +0900

    Title: 트리 노드 제거, Time: 143ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 231208/트리 노드 제거/remove-tree-node.py
+++ 231208/트리 노드 제거/remove-tree-node.py
@@ -2,16 +2,34 @@ from collections import defaultdict, deque
 n = int(input().rstrip())
 parents = list(map(int, input().rstrip().split(" ")))
 rm_node = int(input().rstrip())
-graph = defaultdict(list)
+leafs = set()
+
+is_leaf = [True] * n
+for p in parents:
+    # 부모가 될 수 없는 노드가 리프노드
+    is_leaf[p] = False
 for i in range(n):
-    graph[parents[i]].append(i)
+    if is_leaf[i]:
+        leafs.add(i)
 
-leafs = n - 2 if parents[rm_node] != -1 else n - 1
-q = deque([rm_node])
-while q:
-    node = q.popleft()
-    leafs -= len(graph[node])
-    for nxt in graph[node]:
-        q.append(nxt)
+num_leaf = len(leafs)
 
-print(leafs)
+stack = [rm_node]
+while stack:
+    node = stack.pop()
+    if node in leafs:
+        leafs.remove(node)
+    for i in range(n):
+        if parents[i] == node:
+            stack.append(i)
+if parents[rm_node] != -1:
+    cnt = 0
+    p = parents[rm_node]
+    for i in range(n):
+        if i == rm_node:
+            continue
+        if parents[i] == p:
+             cnt += 1
+    if cnt == 0:
+        leafs.add(parents[rm_node])
+print(len(leafs))
