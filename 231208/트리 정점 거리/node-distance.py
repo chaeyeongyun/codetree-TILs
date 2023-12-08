fc3edc9e112308ed36ce3a7919343131c72da120
chaeyeongyun <70565663+chaeyeongyun@users.noreply.github.com>
2023-12-08 16:48:53 +0900

    Title: 트리 정점 거리, Time: 674ms, Memory: 115MB, Status: Passed - CodeTree

--- 231208/트리 정점 거리/node-distance.py
+++ 231208/트리 정점 거리/node-distance.py
@@ -1,29 +1,44 @@
-from collections import defaultdict
-def dfs(node, target, dist, visited):
-    global n, ans, tree
-    for nxt, d in tree[node]:
-        if not visited[nxt]:
-            visited[nxt] = True
-            if nxt == target:
-                ans = dist + d
-                return
-            dfs(nxt, target, dist + d, visited)
+# 변수 선언 및 입력:
+n, m = tuple(map(int, input().split()))
+edges = [[] for _ in range(n + 1)]
+visited = [False] * (n + 1)
+dist = [
+    [0] * (n + 1)
+    for _ in range(n + 1)
+]
 
-def get_dist(a, b):
-    global n, tree, ans
-    visited = [False] * (n + 1)
-    ans = 0
-    visited[a] = True
-    dfs(a, b, 0, visited)
-
-n, m = map(int, input().rstrip().split(" "))
-tree = defaultdict(list)
-ans = 0
+# n - 1개의 간선 정보를 입력받습니다.
 for _ in range(n - 1):
-    a, b, d = map(int, input().rstrip().split(" "))
-    tree[a].append((b, d))
-    tree[b].append((a, d))
+    x, y, d = tuple(map(int, input().split()))
+
+    # 간선 정보를 인접리스트에 넣어줍니다.
+    edges[x].append((y, d))
+    edges[y].append((x, d))
+
+
+# DFS를 통해 st로부터 모든 정점까지의 거리를 탐색합니다.
+def dfs(st, x):
+    for y, d in edges[x]:
+        # 이미 방문한 노드는 스킵합니다.
+        if visited[y]: 
+            continue
+        
+        visited[y] = True
+
+        # st로부터의 거리를 갱신합니다.
+        dist[st][y] = dist[st][x] + d
+        dfs(st, y)
+
+
+# 각 n개의 정점에 대해, 모든 노드간의 거리를 DFS로 갱신해줍니다.
+for i in range(1, n + 1):
+    for j in range(1, n + 1): 
+        visited[j] = False
+    
+    visited[i] = True
+    dfs(i, i)
+
+# m개의 노드 쌍을 입력받고, 두 노드 쌍 간의 거리를 바로 출력해줍니다.
 for _ in range(m):
-    a, b = map(int, input().rstrip().split(" "))
-    get_dist(a, b)
-    print(ans)
+    x, y = tuple(map(int, input().split()))
+    print(dist[x][y])
