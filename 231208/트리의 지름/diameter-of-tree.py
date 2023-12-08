a474990c7a149bb0a501322eef5bcde1875fc46b
chaeyeongyun <70565663+chaeyeongyun@users.noreply.github.com>
2023-12-08 15:06:03 +0900

    Title: 트리의 지름, Time: 5052ms, Memory: 41MB, Status: Time Limit Exceed - CodeTree

---
+++ 231208/트리의 지름/diameter-of-tree.py
@@ -0,0 +1,22 @@
+from collections import defaultdict
+n = int(input().rstrip())
+graph = defaultdict(list)
+for _ in range(n - 1):
+    a, b, dist = map(int, input().rstrip().split(" "))
+    graph[a].append((b, dist))
+    graph[b].append((a, dist))
+
+def dfs(node, d = 0):
+    global visited, graph, distance
+    for nxt_node, dist in graph[node]:
+        if not visited[nxt_node]:
+            visited[nxt_node] = True
+            distance = max(distance, d + dist)
+            dfs(nxt_node, d + dist)
+
+distance = 0
+for i in range(1, n + 1):
+    visited = [False] * (n + 1)
+    visited[i] = True
+    dfs(i)
+print(distance)
