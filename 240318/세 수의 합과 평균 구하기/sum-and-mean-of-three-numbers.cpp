63a5939be84a0f09ddae3dab7d43384b8986aba9
chaeyeongyun <70565663+chaeyeongyun@users.noreply.github.com>
2024-03-18 12:00:40 +0900

    Title: 세 수의 합과 평균 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240318/세 수의 합과 평균 구하기/sum-and-mean-of-three-numbers.cpp
+++ 240318/세 수의 합과 평균 구하기/sum-and-mean-of-three-numbers.cpp
@@ -5,6 +5,6 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int a, b, c;
     cin >> a >> b >> c;
-    cout << a + b + c << (a + b + c) / 3;
+    cout << a + b + c << endl << (a + b + c) / 3;
     return 0;
 }
