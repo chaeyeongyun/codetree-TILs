53ef7aacc0925e25ed907a5904eaa505cc20d949
chaeyeongyun <70565663+chaeyeongyun@users.noreply.github.com>
2024-03-18 11:11:47 +0900

    Title: 문자열 받아 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240318/문자열 받아 출력/enter-word-and-print.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string> // 문자열 사용하려면 이 패키지 선언해줘야함
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    string a; // 문자열
+    cin >> a;
+    cout << a;
+    return 0;
+}
