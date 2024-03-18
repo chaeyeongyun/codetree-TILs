7d30a41a1c309552a02859ae8f1b7877a08728c5
chaeyeongyun <70565663+chaeyeongyun@users.noreply.github.com>
2024-03-18 13:21:16 +0900

    Title: 체질량지수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240318/체질량지수/bmi.cpp
+++ 240318/체질량지수/bmi.cpp
@@ -2,13 +2,14 @@
 
 int main() {
     // 여기에 코드를 작성해주세요.
-    int height, weight;
-    std::cin >> height, weight;
+    float height, weight;
     int bmi;
+    std::cin >> height >> weight;
+    height /= 100;
     bmi = weight / (height * height);
-    std::cout << bmi;
-    if (bmi > 25) {
-        std::cout << "Obesty";
+    std::cout << bmi << std::endl;
+    if (bmi >= 25) {
+        std::cout << "Obesity";
     }
     return 0;
 }
