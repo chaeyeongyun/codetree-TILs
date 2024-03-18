017db75b4e1ed968b55fcdb8c8048f6e49c5d94b
chaeyeongyun <70565663+chaeyeongyun@users.noreply.github.com>
2024-03-18 11:39:44 +0900

    Title: 1시간 뒤 시간 출력, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240318/1시간 뒤 시간 출력/print-one-hour-later.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    // 특정 문자를 사이에 두고 입력으로 들어가는 경우 사용 가능한 트릭
+    int h, m;
+    std::string e;
+    std::cin >> h >> e >> m;
+    std::cout << h << ":" << m;
+    return 0;
+}
