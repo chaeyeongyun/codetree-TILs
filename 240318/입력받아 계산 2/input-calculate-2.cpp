0f93ea557a97a9ffabfe89acf30628e49f16dbf3
chaeyeongyun <70565663+chaeyeongyun@users.noreply.github.com>
2024-03-18 10:53:22 +0900

    Title: 입력받아 계산 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240318/입력받아 계산 2/input-calculate-2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    // cin은 공백단위로 입력을 받아준다
+    int a, b;
+    cin >> a >> b;
+    cout << a * b;
+    return 0;
+}
