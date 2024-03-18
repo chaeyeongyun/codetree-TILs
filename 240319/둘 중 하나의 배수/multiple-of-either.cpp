19f03cb9cf0a355200adeaabdb9649630f463922
chaeyeongyun <70565663+chaeyeongyun@users.noreply.github.com>
2024-03-19 08:43:48 +0900

    Title: 둘 중 하나의 배수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240319/둘 중 하나의 배수/multiple-of-either.cpp
+++ 240319/둘 중 하나의 배수/multiple-of-either.cpp
@@ -5,7 +5,7 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int a;
     cin >> a;
-    if (a % 3 == 0 or a % 5 == 0)
+    if (a % 3 == 0 || a % 5 == 0)
         cout << 1;
     else
         cout << 0;
