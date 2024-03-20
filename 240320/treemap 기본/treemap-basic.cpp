cc368178aac2f6951af70e13a023c8cb314412bd
chaeyeongyun <70565663+chaeyeongyun@users.noreply.github.com>
2024-03-20 16:09:43 +0900

    Title: treemap 기본, Time: 147ms, Memory: 0MB, Status: Passed - CodeTree

--- 240320/treemap 기본/treemap-basic.cpp
+++ 240320/treemap 기본/treemap-basic.cpp
@@ -1,60 +1,47 @@
 #include <iostream>
-#include <map>
 #include <string>
+#include <map>
+
 using namespace std;
 
+// 변수 선언
+int n;
 map<int, int> m;
 
-void add(int k, int v){
-    // m.insert({k, v});  덮어쓰기 안됨
-    m[k] = v;
-}
-
-void remove(int k){
-    m.erase(k);
-}
-
-void find(int k){
-    if (m.find(k) != m.end())
-        cout << m[k]<< endl;
-    else
-        cout << "None" << endl;
-}
-
-void print_list() {
-    if (m.empty()) {
-        cout << "None" << endl;
-        return;
-    }
-    
-    map<int, int>::iterator it;
-    for (it = m.begin(); it != m.end(); it++) {
-        cout << it -> second << " ";
-    }
-    cout << endl;
-}
-
 int main() {
-    int n;
+    // 입력:
     cin >> n;
-    string ord;
-    int a, b;
-    for (int i = 0; i < n; i++) {
-        cin >> ord;
-        if (ord == string("add")){
-            cin >> a >> b;
-            add(a, b);
+    for(int i = 0; i < n; i++) {
+        string command;
+        cin >> command;
+
+        if(command == "add") {
+            int k, v;
+            cin >> k >> v;
+            m[k] = v;
         }
-        else if (ord == string("find")) {
-            cin >> a;
-            find(a);
+        else if(command == "remove") {
+            int k;
+            cin >> k;
+            m.erase(k);
         }
-        else if (ord == string("remove")) {
-            cin >> a;
-            remove(a);
+        else if(command == "find") {
+            int k;
+            cin >> k;
+            if(m.find(k) == m.end())
+                cout << "None" << endl;
+            else
+                cout << m[k] << endl;
         }
-        else if (ord == string("print_list")) {
-            print_list();
+        else {
+            if(m.empty()) {
+                cout << "None" << endl;
+                continue;
+            }
+            map<int, int>::iterator it;
+            for(it = m.begin(); it != m.end(); it++)
+                cout << it -> second << " ";
+            cout << endl;
         }
     }
     return 0;
