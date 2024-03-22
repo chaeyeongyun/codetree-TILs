f51d393db2223b666aff9ff467e901d77dc79140
chaeyeongyun <70565663+chaeyeongyun@users.noreply.github.com>
2024-03-22 10:00:23 +0900

    Title: 계단 오르기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240322/계단 오르기/climbing-stairs.cpp
+++ 240322/계단 오르기/climbing-stairs.cpp
@@ -3,15 +3,22 @@
 using namespace std;
 
 int dp[MAXN + 1] = {0, 0, 1, 1, 0, };
+
+void print_dp(int n) {
+    for (int i = 0; i <= n;i++) {
+        cout << dp[i] << " ";
+    }
+    cout << endl;
+}
+
 int main() {
     // 여기에 코드를 작성해주세요.
     int n;
     cin >> n;
     for (int i = 4; i <= n; i++) {
-        for (int j = 2; j <= i / 2; j++) {
-            dp[i] += dp[j] * dp[i - j];
-        }
+        dp[i] = dp[i - 2] + dp[i - 3];
     }
-    cout << dp[n];
+    //print_dp(n);
+    cout << dp[n] % 10007;
     return 0;
 }
