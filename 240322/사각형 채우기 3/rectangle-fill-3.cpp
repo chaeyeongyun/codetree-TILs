0dcb2ce210d44931c48d183f35062e09eebac9c0
chaeyeongyun <70565663+chaeyeongyun@users.noreply.github.com>
2024-03-22 10:42:15 +0900

    Title: 사각형 채우기 3, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240322/사각형 채우기 3/rectangle-fill-3.cpp
+++ 240322/사각형 채우기 3/rectangle-fill-3.cpp
@@ -1,14 +1,22 @@
 #include <iostream>
-#define MAXN 1000;
+#define MAXN 1000
 using namespace std;
 
-int dp[] = {0, 2, 7, 0, };
+int dp[MAXN + 1] = {1, 2, 7, 0, };
+
+int sum(int x){
+    int ans = 0;
+    for (int i = 0; i <= x; i++){
+        ans += dp[i];
+    }
+    return ans;
+}
 
 int main() {
     int n;
     cin >> n;
     for (int i = 3; i <= n; i++) {
-        dp[i] = ((dp[i - 1] * dp[1]) + (dp[i - 2] * dp[2])) % 1000000007;
+        dp[i] = ((dp[i - 1] * 2) + (dp[i - 2] * 3) + 2 * sum(i - 3)) % 1000000007;
     }
     cout << dp[n];
     return 0;
