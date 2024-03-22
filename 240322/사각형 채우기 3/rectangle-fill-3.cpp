3d1e604a9bf3ff576a7043bf54442302404d715e
chaeyeongyun <70565663+chaeyeongyun@users.noreply.github.com>
2024-03-22 10:48:50 +0900

    Title: 사각형 채우기 3, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240322/사각형 채우기 3/rectangle-fill-3.cpp
+++ 240322/사각형 채우기 3/rectangle-fill-3.cpp
@@ -7,7 +7,7 @@ int dp[MAXN + 1] = {1, 2, 7, 0, };
 int main() {
     int n;
     cin >> n;
-    for (int i = 3; i <= n; i++) {
+    for (int i = 2; i <= n; i++) {
         dp[i] = (dp[i - 1] * 2 + dp[i - 2] * 3) % 1000000007;
         for (int j = (i - 3); j >= 0; j--) {
             dp[i] = (dp[i] + dp[j] * 2) % 1000000007;
